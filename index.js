5bd64fcc5c1ee39a58ee696b8638f1502cb4f1f7
Dennisgreen7 <denis4167@gmail.com>
2022-04-28 21:11:00 +0300

    conection to MongoDB

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
 const app = express();
+MongoClient.connect('mongodb://localhost:27017', (err, db) => {
+    if (err) {
+        return console.error('Unable to connect to MongoDB server, Error is: ', err);
+    }
+
+    console.log('Connected to MongoDB server');
+});
+//נוסיף מידלוור 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //הגדרות
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '\\index.html');
 });
+app.post('/quotes', (req,res) => {
+    console.log(req.body);
+});
 
 //הפעלה
-
 app.listen(3000, () => {
     console.log('Server is listening on port 3000');
 });
